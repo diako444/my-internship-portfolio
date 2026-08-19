dd2aa884e0d135ad9a532dd20ce1590f3ceb6d96
diako444 <65386351+diako444@users.noreply.github.com>
2026-08-19 09:10:44 +0330

    finalized

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
